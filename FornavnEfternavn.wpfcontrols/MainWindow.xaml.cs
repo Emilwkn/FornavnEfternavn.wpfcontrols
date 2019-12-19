215e24e72a5e350781741a872ba4f102526815e7
Emil Weigelt Kjær Nielsen <emil601b@edu.campusvejle.dk>
2019-12-19 09:41:22 +0100

    Kommer tilbage efter at have lavet flere opgaver.

--- FornavnEfternavn.wpfcontrols/MainWindow.xaml.cs
+++ FornavnEfternavn.wpfcontrols/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace FornavnEfternavn.wpfcontrols
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
